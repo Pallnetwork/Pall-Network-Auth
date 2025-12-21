ebbee072b44f4b50314bf9f740bcf7914b1c5872
Pallnetwork <pallnetworkcommerce@gmail.com>
2025-12-21 18:01:24 +0500

    Update dashboard.tsx

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -705,7 +705,7 @@ export default function Dashboard() {
           {currentPage === "HOME" && (
             <div className="space-y-6">
               <div className="text-center">
-                <h2 className="text-3xl font-bold mb-2">A digital platform for rewards and innovative commerce! 🚀</h2>
+                <h2 className="text-3xl font-bold mb-2"></h2>
                 <p className="text-muted-foreground mb-6">
                 </p>
               </div>
